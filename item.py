724a9a20cfc2197b1845198072b7150c97c1a39f
Edouard440 <edouard.castignoli@efrei.net>
2025-02-25 11:12:30 +0200

    item

--- item.py
+++ item.py
@@ -1,26 +1,32 @@
 import pygame
 from sys import exit
-class Item_heal:
+class Item:
     pygame.init()
-    def __init__(self, x, y, item_type, item_path):
+    def __init__(self, x, y, item_type, item_path,effect):
         self.item_type = item_type
         self.image=  pygame.image.load(item_path)
         self.rect = self.image.get_rect(topleft=(x, y))
         self.collected = False
-        self.x = x
-        self.y = y
-        self.width = width
+        self.effect=effect
+
     def draw(self, screen):
         if not self.collected:
             screen.blit(self.image, self.rect)
+
     def check_collected(self):
-        if self.rect.collidepoint(player_rect) and not self.collected:
+        if self.rect.colliderect(player_rect) and not self.collected:
             self.collected = True
             return True
         return False
 
+    def heal_player_soup(player):
+
+    def tartane(palyer):
+
+    def
+
     items = [
         Item(300, 250, "sword", sword_img),
         Item(500, 350, "gun", gun_img),
-        Item(600, 200, "potion", potion_img)
+        Item(600, 200, "Soup", heal_player_soup )
     ]
