945722f852884a0948204f38eed4d30b87bd5b23
jeanne-lcr <jeanne.leclere@efrei.net>
2025-02-27 01:16:08 +0100

    work on player

--- players.py
+++ players.py
@@ -78,3 +78,31 @@ class Player:
 # players:
 player1 = Player(2, 1, 128, 310)
 player2 = Player(1, 2, 600, 310)
+
+class Playe(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image= pygame.image.load('assets/characters/granny.png').convert_alpha
+        self.rect = self.image.get_rect(midbottom = (200,300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300 :
+            self.gravity= -20
+    
+    def apply_gravity(self):
+        self.gravity +=1
+        self.rect.y += self.gravity
+        if self.rect.bottom >=300:
+            self.rect.bottom = 300
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+
+
+
+
+player = pygame.sprite.GroupSingle()
+player.add(Player())
