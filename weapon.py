3a39d691572b214849b0290fcfac3d2dda652a47
R2y7n <rayane.boukef@efrei.net>
2025-03-01 23:25:59 +0100

    class Weapon modified and start to program the Class Bullet (wil be up tonight)

--- weapon.py
+++ weapon.py
@@ -1,22 +1,37 @@
 import pygame as pg
-import math
+
 
 class Weapon:
-    def __init__(self, name, damage, range, ammo, sprite_pa):
+    def __init__(self, name, damage, fire_range, ammo, sprite_path):
         self.name = name
         self.damage = damage
-        self.range = range
+        self.fire_range = fire_range
         self.ammo = ammo
-        self.sprite = pg.image(sprite_pa)
+        self.sprite = pg.image.load(sprite_path)
+        self.sprite = pg.transform.scale(self.sprite, (50, 50))  # optional resize
 
     def fire(self, pos, direction):
         """gun fire method, to define as function of the weapons that we will set up"""
-        pass
+        if self.ammo > 0:
+            self.ammo -= 1
+            print(f"{self.name} a tiré! Munitions restantes: {self.ammo}")
+        else:
+            print(f"{self.name} n'a plus de munitions!")
 
     def reload(self, amnt):
         """reload method with a specific amount"""
         self.ammo += amnt
+        print(f"{self.name} rechargé avec {amnt} munitions.")
 
-    def draw(self,screen, position):
+    def draw(self, screen, position):
         """Display the weapons on the screen"""
         screen.blit(self.sprite, position)
+
+
+class Bullet:
+    def __init__(self, pos, direction, speed=10, damage=5):
+        self.pos = pg.Vector2(pos)
+        self.direction = direction
+        self.speed = speed
+        self.damage = damage
+        sel.alive
