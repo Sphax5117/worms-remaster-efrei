7184af8df03a2124df85ba108f4759bb28188e5c
Edouard440 <edouard.castignoli@efrei.net>
2025-03-02 16:28:32 +0100

    Item changing

--- item.py
+++ item.py
@@ -1,7 +1,8 @@
 import pygame
+import players
 from sys import exit
+pygame.init()
 class Item:
-    pygame.init()
     def __init__(self, x, y, item_type, item_path,effect):
         self.item_type = item_type
         self.image=  pygame.image.load(item_path)
@@ -14,10 +15,21 @@ class Item:
             screen.blit(self.image, self.rect)
 
     def check_collected(self):
-        if self.rect.colliderect(player_rect) and not self.collected:
+        if self.rect.colliderect(self.rect) and not self.collected:
             self.collected = True
+            self.effect()
             return True
         return False
 
     def heal_player_soup(self):
         self.health=min(self.health+20,100)
+        print(f'Healing {self.health}')
+
+    def pill_boost(self):
+        self.speed+=5
+        self.defense+=5
+        print(f"Speed and defense boosted! Speed: {self.speed}, Defense: {self.defense}")
+
+    pill=Item("pill",,pill_boost)
+    soup=Item("soup",,heal_player_soup)
+    items=[pill,soup]
