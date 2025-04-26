915d3c725a211af7fb016eec07142a4546074f3d
erwannbr <erwann.briot@efrei.net>
2025-04-26 10:18:05 +0200

    to do list

--- python_file/throwables_weapon.py
+++ python_file/throwables_weapon.py
@@ -91,3 +91,14 @@ class SoupPuddle(pg.sprite.Sprite):
 
     def slow_effect(self, player):
         player.speed = max(2, player.speed - 3)
+
+
+### a faire ###
+# - limiter le nombre d'utilisation des armes (genre on peut tirer 3 fois max sinon c'est op) et utiliser que 1 seule armes sur les 45 secondes des tours
+# - afficher l'armes que on est en train d'utiliser sur le player, ou un message en haut de l'écran ou jsp juste que on sache
+# - faire affihcer les sprites qui sont dans assets/items 
+# - BONUS, ajouter l'armes qui eparpille les bullets la, trouver un nom, et demander a tom de faire un sprite 
+# - changer les trajectoires pour en avoir 1 qui est plus droite (celle qui explose pas par exemple) faire genre un snipe pour tirer de loin
+# - BONUS, afficher la trajectoires a l'écran pour que les joueurs puissent viser à l'aide la trajectoire
+# - verifier l'erreur du fait que on ne peut utiliser que 1 des deux armes, les deux sont pareils si on appuis sur 1 ou 2
+# - ajouter des commentaires pas chat sur le programme
